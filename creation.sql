ca4e48985e85f69b2722b74ca37581b54b57269a
Jorge Villalobos <jorge@mozilla.com>
2017-01-19 17:36:39 -0600

    Align creation query with other reports
    The creation query is now cut off at Fridays (like all others), instead
    of using WEEK().

--- creation.sql
+++ creation.sql
@@ -1,18 +1,15 @@
-SET @from_date=DATE_SUB(NOW(), INTERVAL 1 MONTH);
-SELECT created_addons.week AS `Week`, created_addons.total AS `Add-ons`,
-       created_versions.total AS `Versions`
+SET @from_date=DATE_SUB(NOW(), INTERVAL 7 DAY); SET @to_date=NOW();
+SELECT created_addons.total AS `Add-ons`, created_versions.total AS `Versions`
 FROM
-  (SELECT WEEK(a.created) AS `week`, COUNT(DISTINCT(a.id)) AS `total`
+  (SELECT COUNT(DISTINCT(a.id)) AS `total`
    FROM versions AS v
-   JOIN addons AS a ON (
-     v.addon_id = a.id AND a.created >= @from_date AND v.channel = 2 AND
-     a.status NOT IN (0,5,11) AND a.addontype_id != 9)
-   GROUP BY `week`) AS created_addons
-  JOIN
-  (SELECT WEEK(v.created) AS `week`, COUNT(*) AS `total`
+   JOIN addons AS a ON (v.addon_id = a.id)
+   WHERE a.created >= @from_date AND a.created < @to_date AND v.channel = 2 AND
+         a.status NOT IN (0,5,11) AND a.addontype_id != 9
+   ) AS created_addons,
+  (SELECT COUNT(DISTINCT(v.id)) AS `total`
    FROM versions AS v
-   JOIN addons AS a ON (
-     v.addon_id = a.id AND v.created >= @from_date AND v.channel = 2 AND
-     a.status NOT IN (0,5,11) AND a.addontype_id != 9)
-   GROUP BY `week`) AS created_versions
-  ON (created_addons.week = created_versions.week);
+   JOIN addons AS a ON (v.addon_id = a.id)
+   WHERE v.created >= @from_date AND v.created < @to_date AND v.channel = 2 AND
+         a.status NOT IN (0,5,11) AND a.addontype_id != 9
+   ) AS created_versions;
